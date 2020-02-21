14dd11d70cee4efb14924eeae907357a03d16482
Raghunandan.Seshadri <raghubs81@gmail.com>
2020-02-21 19:16:23 +0530

    VSCode: Update images

---
+++ VSCode/build/build.sh
@@ -0,0 +1,9 @@
+if [ ! -d "${HOME}/GitProjects/vscode-dukedark-theme" ]
+then
+   echo "Clone git project : git clone https://github.com/cafeduke/vscode-dukedark-theme.git $HOME/GitProjects/"
+   exit 1
+fi
+
+cd $HOME/GitProjects/vscode-dukedark-theme
+vsce package -o $HOME/GitProjects/Themes/VSCode/build
+code --install-extension $HOME/GitProjects/Themes/VSCode/build/dukedark-0.0.1.vsix
